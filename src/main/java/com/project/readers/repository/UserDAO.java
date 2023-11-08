01975bb9830dd66c9c06cadcf1372b7d7c884a0e
shinjaewon99 <tlswodnjs99@naver.com>
2023-11-08 22:05:50 +0900

    feat : 유저 등록, 수정, 삭제, 로그인 관련 DAO 인터페이스 추가

--- src/main/java/com/project/readers/repository/UserDAO.java
+++ src/main/java/com/project/readers/repository/UserDAO.java
@@ -1,8 +1,16 @@
 package com.project.readers.repository;
 
+import com.project.readers.entity.UserDTO;
+import com.project.readers.entity.UserSessionDTO;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface UserDAO {
+    void register(UserDTO userDTO);
 
+    UserSessionDTO login(UserDTO userDTO);
+
+    void update(UserDTO userDTO);
+
+    void delete(int idNum);
 }

7fbd868ed600081b995f9df0f0b8879021918db8
havegrit <shin_jaean@naver.com>
2023-11-09 03:10:46 +0900

    feat: Implement search by title or author

--- src/main/java/com/project/readers/repository/GalleryDAO.java
+++ src/main/java/com/project/readers/repository/GalleryDAO.java
@@ -9,6 +9,8 @@ import com.project.readers.entity.GalleryDTO;
 @Mapper
 public interface GalleryDAO {
 	List<GalleryDTO> getList(GalleryDTO galleryDTO);
+	List<GalleryDTO> searchByTitle(GalleryDTO galleryDTO);
+	List<GalleryDTO> searchByAuthor(GalleryDTO galleryDTO);
 	GalleryDTO getItem(GalleryDTO galleryDTO);
 	void saveGallery(GalleryDTO galleryDTO);
 	Integer getGalleryCount();

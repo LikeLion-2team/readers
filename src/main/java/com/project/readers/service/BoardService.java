0fe5c45f7d84abc4727585e693f62cd95ebc3555
yunhuiGwak <kwakyh17@naver.com>
2023-11-08 23:55:43 +0900

    feat: 게시판 로직 구현 BoardService.java - 게시판 CRUD 로직 구현

--- src/main/java/com/project/readers/service/BoardService.java
+++ src/main/java/com/project/readers/service/BoardService.java
@@ -1,13 +1,42 @@
 package com.project.readers.service;
 
-import org.springframework.stereotype.Service;
-
+import com.project.readers.entity.BoardDTO;
 import com.project.readers.repository.BoardDAO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import jakarta.annotation.Resource;
+import java.util.List;
 
 @Service
+@RequiredArgsConstructor
+@Transactional
 public class BoardService {
-	@Resource(name="boardDAO")
-	private BoardDAO boardDAO;
+
+    private final BoardDAO boardDAO;
+
+    public void createBoard(BoardDTO boardDTO) {
+
+        boardDAO.createBoard(boardDTO);
+    }
+
+    public Integer getBoardCount(BoardDTO boardDTO){
+        return boardDAO.getBoardCount(boardDTO);
+    }
+    public List<BoardDTO> getBoardList(BoardDTO boardDTO){
+
+		return boardDAO.getBoardList(boardDTO);
+	}
+
+    public BoardDTO getBoardOne(Integer boardNum) {
+        return boardDAO.getBoardOne(boardNum);
+    }
+
+    public void deleteBoard(Integer boardNum) {
+        boardDAO.deleteBoard(boardNum);
+    }
+
+    public void updateBoard(BoardDTO boardDTO) {
+        boardDAO.updateBoard(boardDTO);
+    }
 }

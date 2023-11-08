6c935385d3eea86e43e133cd3144bb3ae3bc58e8
shinjaewon99 <tlswodnjs99@naver.com>
2023-11-08 22:10:13 +0900

    feat : 세션 정보 관련 DTO 클래스 추가

--- src/main/java/com/project/readers/entity/UserSessionDTO.java
+++ src/main/java/com/project/readers/entity/UserSessionDTO.java
@@ -2,16 +2,10 @@ package com.project.readers.entity;
 
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
-@ToString
 @Getter
 @Setter
 public class UserSessionDTO {
-	 
-	 //아이디
-	 private String id;
-	 //권한 레벨
-	 private int roleNum;
-	 
+    private String id;
+    private int roleNum;
 }

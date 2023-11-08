e725c000702bbda8c870c13538a84e5e3a5900d6
yunhuiGwak <kwakyh17@naver.com>
2023-11-09 08:54:36 +0900

    feat: 댓글서비스 로직에서 세션 검증 ReplyService.java - 비즈니스 로직에서 세션에서 가져온 아이디로 검증하게 설정

--- src/main/java/com/project/readers/service/ReplyService.java
+++ src/main/java/com/project/readers/service/ReplyService.java
@@ -1,19 +1,28 @@
 package com.project.readers.service;
 
-import com.project.readers.entity.BoardDTO;
+import com.project.readers.config.SessionConfig;
 import com.project.readers.entity.ReplyDTO;
 import com.project.readers.repository.ReplyDAO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class ReplyService {
 
     private final ReplyDAO replyDAO;
 
+    public void createReply(ReplyDTO replyDTO) throws SecurityException{
+        if (SessionConfig.getSessionDTO().getId() == null) {
+            throw new SecurityException("회원만 작성할 수 있습니다.");
+        }
+//        if ((replyDTO.getRplNum() == null || "".equals(replyDTO.getRplNum())) && SessionConfig.getSessionDTO().getId() != null)
+            replyDAO.createReply(replyDTO);
+    }
     public List<ReplyDTO> getReplyList(Integer boardNum) {
         return replyDAO.getReplyList(boardNum);
     }
@@ -22,17 +31,22 @@ public class ReplyService {
         return replyDAO.getReplyOne(rplNum);
     }
 
-    public void createReply(ReplyDTO replyDTO) {
-        if (replyDTO.getRplNum() == null || "".equals(replyDTO.getRplNum()))
-            replyDAO.createReply(replyDTO);
-    }
+    public void updateReply(ReplyDTO replyDTO) throws SecurityException {
+        String currentUserId = SessionConfig.getSessionDTO().getId();
+        String creatorId = replyDTO.getCreator();
 
-    public void updateReply(ReplyDTO replyDTO) {
-        if(!(replyDTO.getRplNum() == null || "".equals(replyDTO.getRplNum())))
+        if (currentUserId == null || !currentUserId.equals(creatorId)) {
+            throw new SecurityException("게시물의 작성자만 수정할 수 있습니다.");
+        }
+//        if(!(replyDTO.getRplNum() == null || "".equals(replyDTO.getRplNum())))
             replyDAO.updateReply(replyDTO);
     }
 
-    public void deleteReply(Integer rplNum) {
+    public void deleteReply(Integer rplNum) throws SecurityException{
+        ReplyDTO replyDTO = replyDAO.getReplyOne(rplNum);
+        if (!replyDTO.getCreator().equals(SessionConfig.getSessionDTO().getId())) {
+            throw new SecurityException("게시물의 작성자만 삭제할 수 있습니다.");
+        }
         replyDAO.deleteReply(rplNum);
     }
 }
